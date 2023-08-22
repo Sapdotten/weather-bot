da6e69baf248091e714e213776f959a6e85b41fd
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-08-22 11:55:56 +0400

    change yaml and requirements

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
 from aiogram import Bot
-#from aiogram.methods.delete_webhook import DeleteWebhook
+# from aiogram.methods.delete_webhook import DeleteWebhook
 from aiogram import Dispatcher
 from handlers.commands import register_bot
 from data_manager.data_manager import start
 from handlers.wearing import wearing_router
 import os
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from modules.scheduler import start_scheduler
 from handlers.commands import command_router
 from aiogram.client.session.aiohttp import AiohttpSession
@@ -20,8 +20,9 @@ async def main() -> None:
     """
     Entry point
     """
-    #load_dotenv('.env')
-    #TOKEN_API = os.getenv('TOKEN_API')
+    print('Program starts')
+    # load_dotenv('.env')
+    # TOKEN_API = os.getenv('TOKEN_API')
     TOKEN_API = os.environ["TOKEN_API"]
     print('token_api is', TOKEN_API)
     session = AiohttpSession()

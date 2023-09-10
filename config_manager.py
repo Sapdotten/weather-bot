1ed4f843d226e4dbb8f9266caa6ae8765a16ede8
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-09-10 04:27:31 +0400

    Создание менеджера конфигов

---
+++ config_manager.py
@@ -0,0 +1,44 @@
+import yaml
+import os
+from dotenv import load_dotenv
+
+global config
+
+
+def start():
+    global config
+    print('config_manager starts')
+    with open('configs.yaml', 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+
+def token() -> str:
+    if config['env_mode'] == 'getenv':
+        load_dotenv('.env')
+        return os.getenv('TOKEN_API')
+    elif config['env_mode'] == 'environ':
+        return os.environ["TOKEN_API"]
+
+
+def db_file():
+    return config['db_file']
+
+
+def timezone():
+    return config['time_zone']
+
+
+class logger:
+    global config
+
+    @classmethod
+    def level(cls):
+        return config['logging']['level']
+
+    @classmethod
+    def file(cls):
+        return config['logging']['file']
+
+    @classmethod
+    def output(cls):
+        return config['logging']['out']

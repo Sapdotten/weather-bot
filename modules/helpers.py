7863753d03142e03c8c4e12230c239194e3e3486
Sapdotten <semenova.a.p.04@gmail.com>
2023-12-30 04:13:08 +0400

    Added a transliteration from eng to rus

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,14 +1,19 @@
 from transliterate import translit
 
+
 async def incr_time(offset: int, time: str) -> str:
     time = time.split(':')
     print(f'offset is {offset}')
-    offset =int(time[0])-offset
+    offset = int(time[0]) - offset
     offset = offset % 24
     print(f'final hour is: {offset}')
     time = str(offset) + ':' + time[1]
     return time
 
 
-async def translite(ru_text: str) -> str:
+async def translite_ru_to_eng(ru_text: str) -> str:
     return translit(ru_text, language_code='ru', reversed=True)
+
+
+async def translite_eng_to_ru(ru_text: str) -> str:
+    return translit(ru_text, language_code='ru')
